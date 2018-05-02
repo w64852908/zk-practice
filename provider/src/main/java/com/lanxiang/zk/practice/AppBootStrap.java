cc7be5916113be911f090550c5904888e9b6d5b8
兰兢 <lanjing02@maoyan.com>
2018-05-02 16:59:35 +0800

    init project struct

--- provider/src/main/java/com/lanxiang/zk/practice/AppBootStrap.java
+++ provider/src/main/java/com/lanxiang/zk/practice/AppBootStrap.java
@@ -1,10 +1,21 @@
 package com.lanxiang.zk.practice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
 /**
  * Created by lanxiang on 2018/5/2.
  */
 
-
+@SpringBootApplication
+@ComponentScan(basePackages = "com.lanxiang.zk.practice")
+@ImportResource(locations = {"classpath:applicationContext.xml"})
 public class AppBootStrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBootStrap.class, args);
+    }
 
 }
