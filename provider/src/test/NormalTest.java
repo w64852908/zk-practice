a304cbe9068d823233c2e6db0be8b45c03ca14ff
兰兢 <lanjing02@maoyan.com>
2018-05-31 20:46:49 +0800

    update

--- provider/src/test/NormalTest.java
+++ provider/src/test/NormalTest.java
@@ -1,8 +1,8 @@
 import java.lang.reflect.Field;
 
-import com.lanxiang.zk.practice.service.annotation.ZkConfig;
 import org.junit.Test;
 
+import com.lanxiang.zk.practice.service.annotation.ZkConfig;
 import com.lanxiang.zk.practice.service.executetest.zkconfig.LanxiangConfig;
 
 /**
@@ -26,17 +26,36 @@ public class NormalTest {
     }
 
     @Test
-    public void test2() throws Exception {
-        Field field = Student.class.getDeclaredField("name");
-        field.setAccessible(true);
-        System.out.println(field.getName());
-        field.set(Student.class, "lanxiang");
-        System.out.println(Student.getName());
+    public void test3() throws Exception {
+        System.out.println(LanxiangConfig.class.getDeclaredField("name").getAnnotation(ZkConfig.class).key());
     }
 
     @Test
-    public void test3() throws Exception {
-        System.out.println(LanxiangConfig.class.getDeclaredField("name").getAnnotation(ZkConfig.class).key());
+    public void test2() throws Exception {
+        Field field = Student.class.getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(Student.class, "lanxiang");
+        System.out.println(Student.getName());
+        System.out.println(new Student());
+    }
+}
+
+class Student {
+
+    private static String name;
+
+    private static Integer no;
+
+    public Student() {
+        System.out.println("constructed");
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static Integer getNo() {
+        return no;
     }
 }
 
@@ -64,21 +83,3 @@ class User {
 }
 
 
-class Student {
-
-    private static String name;
-
-    private static Integer no;
-
-    public Student() {
-        System.out.println("constructed");
-    }
-
-    public static String getName() {
-        return name;
-    }
-
-    public static Integer getNo() {
-        return no;
-    }
-}
